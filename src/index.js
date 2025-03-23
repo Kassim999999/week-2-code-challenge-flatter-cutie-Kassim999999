977b16017bf9a27661d6892c7e60bdfccea069d3
Kassim <kassimrooney@gmail.com>
2025-03-23 10:52:26 +0300

    Created span(element) and Fetched characters

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 // Your code here
+let base_url = "http://localhost:3000/characters"
+
+const characterBar = document.getElementById('character-bar');
+
+fetch(base_url)
+.then(res => res.json())
+.then(characters => characters.forEach(character => {
+    
+    let span = document.createElement("span");
+    span.textContent = character.name;
+    span.addEventListener('click', () => characterDetails(character));
+    characterBar.appendChild(span);
+}))
+
+.catch(err => console.log(err)
+)
